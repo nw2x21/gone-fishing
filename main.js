a306db1e5ca2a9901c305994ba07ed884221bb88
treasure <nwadminmain@nw2xmediapros.com>
2022-03-03 17:16:35 -0500

    commit1

---
+++ main.js
@@ -0,0 +1,20 @@
+const prompt = require( 'prompt-sync') ({sigint: true});
+//Step 1: Foundation//
+
+let time = 0;
+let availFish = [""];
+let hour = 6;
+let combinedweight = 0;
+let combinedvalue = 0;
+
+//Create Fish Randomizer
+
+function Radomizer(params) {
+    let availFish = ["Salmon", "Trout", "Bass", "Crappie", "Perch", "Snapper", "Tuna", "Walleye", "Grouper", "Halibut" ];
+    let descript1 = ["Teeny", "Colossal", "Bumpy", "Grand", "Dragon", "King", "Mid-Sized"]
+    let descript2 = ["Oily", "Flakey", "Striped", "Yellow", "Blue", "Red", "Rainbow"]
+}
+
+
+
+//(let i = 1; i < 10; i++)
